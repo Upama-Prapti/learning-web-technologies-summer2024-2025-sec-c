ba2577b77f9863a9b6d58f9407ab0478843ba310
Upama Prapti <cpcourse9806@gmail.com>
2025-08-02 17:11:30 +0600

    -m

--- Lab4/Calculator.js
+++ Lab4/Calculator.js
@@ -1,17 +1,41 @@
+
+let selectedOperator;
+
+
+document.getElementById('plus').addEventListener('click', function() {
+    selectedOperator = '+';
+});
+
+document.getElementById('minus').addEventListener('click', function() {
+    selectedOperator = '-';
+});
+
+document.getElementById('multiplication').addEventListener('click', function() {
+    selectedOperator = '*';
+});
+
+document.getElementById('division').addEventListener('click', function() {
+    selectedOperator = '/';
+});
+
+document.getElementById('percentage').addEventListener('click', function() {
+    selectedOperator = '%';
+});
+
+
+
 function calculate() {
     let num1 = Number(document.getElementById('num1').value);
- 
-    let op = selectedOperator; 
     let num2 = Number(document.getElementById('num2').value);
+    let op = selectedOperator; 
     let res;
 
    
     if (isNaN(num1) || isNaN(num2) || !op) {
-        document.getElementById('result').value = 'Not a valid input';
+        document.getElementById('result').value = 'Invalid Input';
         return;
     }
 
- 
     if (op == '+') {
         res = num1 + num2;
     } else if (op == '-') {
@@ -20,7 +44,7 @@ function calculate() {
         res = num1 * num2;
     } else if (op == '/') {
         if (num2 === 0) {
-            document.getElementById('result').value = 'Cannot divide by zero';
+            document.getElementById('result').value = 'Cannot divide by 0';
             return;
         }
         res = num1 / num2;
@@ -38,14 +62,18 @@ function calculate() {
     document.getElementById('result').value = res;
 }
 
+
 function gayeb() {
     document.getElementById('num1').value = '';
     document.getElementById('num2').value = '';
     document.getElementById('result').value = '';
+    selectedOperator = null;
 }
 
 
 document.getElementById('calculateBtn').addEventListener('click', calculate);
+
+document.getElementById('clean').addEventListener('click', gayeb);
 
 
 document.getElementById('num1').addEventListener('input', () => {
